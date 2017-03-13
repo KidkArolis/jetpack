9f8097d4e4e928f1702dd68ff2ccd57f2af4eff6
Karolis Narkevicius <karolis.n@gmail.com>
2017-03-13 20:59:00 +0000

    Take path as an argument, fix #2

--- bin/jetpack.js
+++ bin/jetpack.js
@@ -4,9 +4,14 @@ const program = require('commander')
 
 program
   .version(require('../package.json').version)
+  .arguments('[dir]')
   .option('-p, --port <n>', 'Port, defaults to 3000', Number)
   .option('-j, --jsx <pragma>', 'Specify jsx pragma, defaults to React.createElement or Preact.h if preact is installed')
   .parse(process.argv)
+
+if (program.args[0]) {
+  process.chdir(program.args[0])
+}
 
 require('../server/server')({
   port: program.port,
