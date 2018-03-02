76e81774ecec1e9149e20eae1195d144b37bd1e3
Karolis Narkevicius <karolis.n@gmail.com>
2018-03-02 16:02:31 +0000

    Fix linter and tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const env = process.env.NODE_ENV || 'development'
-
 module.exports = function (options) {
   let config = {
     entry: {
