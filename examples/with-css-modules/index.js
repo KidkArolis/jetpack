f00dfe77b4ca1d5f98de3ce1dc67b64c1f65148e
Karolis Narkevicius <karolis.n@gmail.com>
2018-03-09 23:29:13 +0000

    Add css modules example

---
+++ examples/with-css-modules/index.js
@@ -0,0 +1,15 @@
+const styles = require('./styles.module.css')
+
+function main () {
+  document.querySelector('#root').innerHTML = `
+    <div class=${styles.container}>
+      <h1>HMR!</h1>
+      <p>Hello world.</p>
+      <p>Paragraph two.</p>
+    </div>
+  `
+}
+
+main()
+
+module.hot.accept()
