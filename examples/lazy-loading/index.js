2e0ac173e65c553f42a94436f6524dd721f231c1
Karolis Narkevicius <karolis.n@gmail.com>
2018-11-03 18:08:00 +0000

    Add lazy loading example to excerise split chunks of js and css

---
+++ examples/lazy-loading/index.js
@@ -0,0 +1,23 @@
+require('./styles.css')
+
+function main () {
+  document.querySelector('#root').innerHTML = `
+    <h1>HMR!</h1>
+    <p>Hello world.</p>
+    <p>Paragraph two.</p>
+    <button id='load'>Load more</button>
+  `
+
+  document.querySelector('#load').addEventListener('click', () => {
+    require(['./more.js'], (more) => {
+      const p = Array.from(document.querySelectorAll('p'))
+      p[p.length - 1].appendChild(more())
+    })
+  })
+}
+
+main()
+
+if (module.hot) {
+  module.hot.accept()
+}
