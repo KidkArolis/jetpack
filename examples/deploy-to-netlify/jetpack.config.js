034f68481c3b72843c9ef816360f4c96efb7d2b9
Karolis Narkevicius <karolis.n@gmail.com>
2018-11-13 22:24:41 +0000

    Fix deploy-to-netlify example

--- examples/deploy-to-netlify/jetpack.config.js
+++ examples/deploy-to-netlify/jetpack.config.js
@@ -6,15 +6,6 @@ module.exports = {
     modules: true
   },
 
-  hooks: {
-    afterBuild (options) {
-      console.log('Writing', path.join(options.dir, options.dist, '_redirects'))
-      fs.writeFileSync(path.join(options.dir, options.dist, '_redirects'), `
-        /*    /index.html   200
-      `)
-    }
-  },
-
   webpack (config, options) {
     config.resolve = {
       extensions: ['.js', '.css']
