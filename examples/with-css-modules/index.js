97a264601d8419ed8d95b8b144c85d496e0f34e1
Karolis Narkevicius <karolis.n@gmail.com>
2019-05-24 10:58:08 +0100

    Quote class or else this example is misleading

--- examples/with-css-modules/index.js
+++ examples/with-css-modules/index.js
@@ -2,7 +2,7 @@ const styles = require('./styles.css')
 
 function main () {
   document.querySelector('#root').innerHTML = `
-    <div class=${styles.container}>
+    <div class="${styles.container}">
       <h1>HMR!</h1>
       <p>Hello world.</p>
       <p>Paragraph two.</p>
