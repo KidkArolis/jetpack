884723b315ff24ca3ac5f5a1fa93d745d6101d80
Sagi Medina <sagi.medina@alibaba-inc.com>
2019-11-14 18:38:59 +0200

    remove warning msg from test results and update tests snapshots

--- test/build.test.js
+++ test/build.test.js
@@ -69,7 +69,7 @@ async function build (t, pkg) {
     extendEnv: false
   })
 
-  t.snapshot(result.all.replace(/^jetpack › Built in.*$/mg, '').split('\n').sort().join('\n'), `jetpack output for compiling ${pkg}`)
+  t.snapshot(result.all.replace(/^jetpack › Built in.*$/mg, '').replace(/^warning: .*$/mg, '').split('\n').sort().join('\n'), `jetpack output for compiling ${pkg}`)
 
   if (result.exitCode !== 0) {
     console.log('Failed to build')
